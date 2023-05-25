42f160bd3769f9268c15f717a757f420a741e9c0
Tim Huang <taxihuang030926@gmail.com>
2023-05-25 11:58:54 +0800

    5/25 lecture progress

--- tetris/main.c
+++ tetris/main.c
@@ -1,6 +1,10 @@
+// TODO Fix render and clear 
 // define constants
 #define CANVAS_HEIGHT 20
 #define CANVAS_WIDTH 10
+#define RENDER_DELAY 100
+#define FALL_DELAY 500
+
 #define LEFT_KEY 0x25
 #define RIGHT_KEY 0x27
 #define ROTATE_KEY 0x26
@@ -125,7 +129,7 @@ bool move(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], int originalX, int original
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < size; j++) {
 			if (shapeData.rotates[newRotate][i][j]) {
-				if (newX + j < 0 || newX + j >= CANVAS_WIDTH || newY + i >= CANVAS_HEIGHT) {
+				if (newX + j < 0 || newX + j >= CANVAS_WIDTH || newY + i < 0 || newY + i >= CANVAS_HEIGHT) {
 					return false;
 				}
 				if (!canvas[newY + i][newX + j].current && canvas[newY + i][newX + j].shape != EMPTY) {
@@ -200,16 +204,83 @@ void printCanvas(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State* state) {
 	for (int i = 0; i < CANVAS_HEIGHT; i++) {
 		printf("|");
 		for (int j = 0; j < CANVAS_WIDTH; j++) {
-			printf("\033[%dm\u3000", canvas[i][j].color);
+			printf("\033[48;5;%dm\u3000\033[0m", canvas[i][j].color);
 		}
-		printf("\033[0m|\n");
+		printf("\x1b[0m|\n");
 	}
+
+	// print next tetrimino
+	Shape shapeData = shape[state->queue[1]];
+	printf("\033[%d;%dHNEXT:", 3, CANVAS_WIDTH * 2 + 5);
+	for (int i = 1; i <= 4; i++) {
+		shapeData = shape[state->queue[i]];
+		for (int j = 0; j < 4; j++) {
+			printf("\033[%d;%dH", i * 4 + j, CANVAS_WIDTH * 2 + 15);
+			for (int k = 0; k < 4; k++) {
+				if (j < shapeData.size && k < shapeData.size && shapeData.rotates[0][j][k]) {
+					printf("\033[48;5;%dm\u3000\033[0m", shapeData.color);
+				}
+				else {
+					printf("\x1b[0m ");
+				}
+			}
+		}
+	}
+
+	printf("\033[%d;%dHSCORE: %10d\n", CANVAS_HEIGHT, CANVAS_WIDTH * 2 + 5, state->score);
 	return;
 }
 
 void logic(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State* state) {
-	if (move(canvas, state->x, state->y, state->rotate, state->x, state->y + 1, state->rotate, state->queue[0]))
-		state->y++;
+	if (ROTATE_FUNC()) {
+		int newRotate = (state->rotate + 1) % 4;
+		if (move(canvas, state->x, state->y, state->rotate, state->x, state->y, newRotate, state->queue[0])) {
+			state->rotate = newRotate;
+		}
+	}
+	else if (LEFT_FUNC()) {
+		if (move(canvas, state->x, state->y, state->rotate, state->x, state->y, state->rotate, state->queue[0])) {
+			state->x--;
+		}
+	}
+	else if (RIGHT_FUNC()) {
+		if (move(canvas, state->x, state->y, state->rotate, state->x, state->y, state->rotate, state->queue[0])) {
+			state->x++;
+		}
+	}
+	else if (SOFT_DROP_FUNC()) {
+		state->falltime = FALL_DELAY;
+	}
+	else if (HARD_DROP_FUNC()) {
+		state->falltime = FALL_DELAY * CANVAS_HEIGHT;
+	}
+
+	state->falltime += RENDER_DELAY;
+	while (state->falltime >= FALL_DELAY) {
+		state->falltime -= FALL_DELAY;
+
+		if (move(canvas, state->x, state->y, state->rotate, state->x, state->y + 1, state->rotate, state->queue[0])) {
+			state->y++;
+		}
+		else {
+			state->score += clearLine(canvas) * 100;
+
+			state->x = CANVAS_WIDTH / 2;
+			state->y = 0;
+			state->rotate = 0;
+			state->falltime = 0;
+			state->queue[0] = state->queue[1];
+			state->queue[1] = state->queue[2];
+			state->queue[2] = state->queue[3];
+			state->queue[3] = rand() % 7;
+
+			if (!move(canvas, state->x, state->y, state->rotate, state->x, state->y, state->rotate, state->queue[0])) {
+				printf("\033[%d;%dH\x1b[41m GAME OVER \x1b[0m\033[%d;%dH", CANVAS_HEIGHT - 3, CANVAS_WIDTH * 2 + 5, CANVAS_HEIGHT + 5, 0);
+			}
+		}
+	}
+
+	
 	return;
 }
 
@@ -223,10 +294,6 @@ int main() {
 		.falltime = 0
 	};
 
-	for (int i = 0; i < 5; i++) {
-		state.queue[i] = rand() % 7;
-	}
-
 	Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH];
 	for (int i = 0; i < CANVAS_HEIGHT; i++) {
 		for (int j = 0; j < CANVAS_WIDTH; j++) {
@@ -234,15 +301,19 @@ int main() {
 		}
 	}
 
+	for (int i = 0; i < 5; i++) {
+		state.queue[i] = rand() % 7;
+	}
+
 	CLS();
-	printf("\e[?251");
+	printf("\e[?25l");
 
 	move(canvas, state.x, state.y, state.rotate, state.x, state.y, state.rotate, state.queue[0]);
 
 	while (1) {
 		logic(canvas, &state);
 		printCanvas(canvas, &state);
-		SLEEP(100);
+		SLEEP(RENDER_DELAY);
 	}
 	return 0;
 }
